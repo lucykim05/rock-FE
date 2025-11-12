8675da3f4d716aae40600ff426926c534e79ecb0
stellalee1210 <yeobinlee1210@gmail.com>
2025-11-12 19:37:41 +0900

    feat(Error): 사용자/채널 메세지 전송 실패 에러 추가

--- error/Errors.js
+++ error/Errors.js
@@ -6,3 +6,19 @@ export class StudyTimeCountError extends BotError {
     super(ERROR_MESSAGES.ERROR_STUDY_TIME_CONUT);
   }
 }
+
+export class SendingDMFailError extends BotError {
+  constructor(state) {
+    console.log(
+      `[${state.member.user.displayName}] 사용자에게 ${ERROR_MESSAGES.ERROR_SENDING_MESSAGE_FAIL}`
+    );
+  }
+}
+
+export class SendingChannelMessageFailError extends BotError {
+  constructor(state) {
+    console.log(
+      `[${state.channelId}] 채널에 ${ERROR_MESSAGES.ERROR_SENDING_MESSAGE_FAIL}`
+    );
+  }
+}
