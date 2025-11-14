92474464982b9c01ac5b1786a0eb5a5d89925aa0
stellalee1210 <yeobinlee1210@gmail.com>
2025-11-14 22:02:14 +0900

    Revert "feat(Error): 공부시간 측정 에러 메세지 추가"
    This reverts commit 340d1a1055c77785a335cd8c1b0a531f167abbf2.

--- error/Errors.js
+++ error/Errors.js
@@ -2,9 +2,8 @@ import { BotError } from "./BotError.js";
 import { ERROR_MESSAGES } from "../constants/errorMessages.js";
 
 export class StudyTimeCountError extends BotError {
-  constructor(userId) {
+  constructor() {
     super(ERROR_MESSAGES.ERROR_STUDY_TIME_CONUT);
-    console.log(`${userId}${ERROR_MESSAGES.ERROR_STUDY_TIME_CONUT}`);
   }
 }
 
