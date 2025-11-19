6db6f45d40f82e1d33f0cf948c5b2b4cba316ec9
stellalee1210 <yeobinlee1210@gmail.com>
2025-11-19 23:02:03 +0900

    refactor: 커스텀 에러 로직 수정

--- error/Errors.js
+++ error/Errors.js
@@ -8,26 +8,25 @@ export class StudyTimeCountError extends BotError {
 }
 
 export class SendingDMFailError extends BotError {
-  constructor(state, error) {
+  constructor() {
     super(ERROR_MESSAGES.ERROR_SENDING_MESSAGE_FAIL);
-    console.log(
-      `[${state.member.user.displayName}] 사용자에게 ${ERROR_MESSAGES.ERROR_SENDING_MESSAGE_FAIL}\n${error.message}`
-    );
   }
 }
 
 export class SendingChannelMessageFailError extends BotError {
-  constructor(state, error) {
+  constructor() {
     super(ERROR_MESSAGES.ERROR_SENDING_MESSAGE_FAIL);
-    console.log(
-      `[${state.channelId}] 채널에 ${ERROR_MESSAGES.ERROR_SENDING_MESSAGE_FAIL}\n${error.message}`
-    );
   }
 }
 
 export class SaveStudyTimeToDBFailError extends BotError {
-  constructor(error) {
+  constructor() {
     super(ERROR_MESSAGES.ERROR_STUDY_TIME_DBSAVE_FAIL);
-    console.log(`DB에 공부시간 저장 실패\n${error.message}`);
+  }
+}
+
+export class GetStudyTimeError extends BotError {
+  constructor() {
+    super(ERROR_MESSAGES.ERROR_NO_STUDY_TIME);
   }
 }
