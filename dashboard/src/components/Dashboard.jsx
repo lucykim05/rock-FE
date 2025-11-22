b47926fc20bdb7957c4825eb182c8ca0f65e98cf
stellalee1210 <yeobinlee1210@gmail.com>
2025-11-22 15:39:07 +0900

    feat(getGuild): API 요청을 통해 해당 사용자가 속한 모든 서버 정보 받는 로직 추가

--- dashboard/src/components/Dashboard.jsx
+++ dashboard/src/components/Dashboard.jsx
@@ -1,14 +1,15 @@
-import { PersonalStatsCard } from './PersonalStatsCard';
-import { RankingCard } from './RankingCard';
-import GuildDropdown from './GuildDropdown';
-import AttendanceCalendar from './AttendanceCalendar';
-
+import { PersonalStatsCard } from './PersonalStatsCard.jsx';
+import { RankingCard } from './RankingCard.jsx';
+import GuildDropdown from './GuildDropdown.jsx';
+import AttendanceCalendar from './AttendanceCalendar.jsx';
+import { useEffect, useRef } from 'react';
+import { getGuildInfo  } from '../guildDropdown/getGuild.js';
 
 export default function Dashboard (){
     const guildId = '1435586389243854860';
     const userId = '391098361924812800';
     const userName = '유저이름';
-      
+
     // mock 데이터
     const personalStats = { attendanceCount: 3, streakDays: 2, studyTotal: 45 };
     const rankingData = [
@@ -16,7 +17,15 @@ export default function Dashboard (){
         { username: '희주', hours: 38 },
         { username: '돌쇠', hours: 32 },
         { username: '이슬', hours: 30 },
-  ];
+    ];
+
+    //한 번만 실행되게 useRef 사용
+    const hasFetchedGuild = useRef(false);
+    useEffect(()=>{
+        getGuildInfo(hasFetchedGuild);
+    }, [])
+
+
     return (
         <main className="px-4 pt-[120px] pb-6 md:px-8 lg:px-40">
         {/* 유저네임 + 드롭다운 */}
