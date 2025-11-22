289e93b3e0421da892721d9b02407443547e926a
stellalee1210 <yeobinlee1210@gmail.com>
2025-11-22 09:57:32 +0900

    refactor(loginFunctions): access token 발급 로직 파일 분리

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -6,8 +6,7 @@ import Header from './components/Header';
 import { PersonalStatsCard } from './components/PersonalStatsCard';
 import { RankingCard } from './components/RankingCard';
 import { DISCORD_LOGIN_URL } from './constants/constant.js';
-import { supabase } from "./database/supabaseClient.js";
-
+import { code2AccessToken } from './login/loginFunctions.js';
 export default function App() {
   const guildId = '1435586389243854860';
   const userId = '391098361924812800';
@@ -19,7 +18,7 @@ export default function App() {
   //로그인 버튼 클릭 시 디스코드 로그인하는 화면으로 이동
   const handleDiscordLogin = () =>{
     console.log('디스코드 로그인 실행')
-    window.location.href = DISCORD_LOGIN_URL;
+    window.location.href = DISCORD_LOGIN_URL; 
   }
   
   //로그아웃 버튼 클릭시 logainState false 및 localStorage에 저장된 access_token 삭제
@@ -32,32 +31,11 @@ export default function App() {
   const hasRequestedAccessToken = useRef(false); // 이미 요청했는지 체크
   useEffect(() => {
     const handleLogin = async () => {
-    const urlParams = new URLSearchParams(window.location.search);
-    const code = urlParams.get('code');
-
-    //url에 code가 있고 아직 supabase에 요청을 안 했을 때만 실행
-    if ( code && !hasRequestedAccessToken.current) {
-      hasRequestedAccessToken.current = true;
-      const { data, error } = await supabase.functions.invoke("discord-oauth", {
-        body: { code },
-      });
-
-      if (error) {
-        console.error("에러 발생:", error);
-        alert("로그인에 실패했습니다");
-        // 에러가 나면 code는 사용하지 못하기 때문에 삭제
-        window.history.replaceState({}, document.title, "/"); 
-      } else {
-        console.log("로그인 성공!");
-        
-        localStorage.setItem('discord_access_token', data.access_token);
+      const url = window.location.search;
+      const isConvertSuccess = await code2AccessToken(url, hasRequestedAccessToken);
+      if(isConvertSuccess)
         setLoginState(true);
-
-        // 페이지를 새로고침 하지 않고 url에 code만 지운 상태로 변경 
-        window.history.replaceState({}, document.title, window.location.pathname);
-      }
     };
-  };
 
   handleLogin();
 
---
+++ dashboard/src/login/loginFunctions.js
@@ -0,0 +1,29 @@
+import { supabase } from "../database/supabaseClient.js";
+
+export const code2AccessToken = async (url, hasRequestedAccessToken) => {
+  const urlParams = new URLSearchParams(url);
+  const code = urlParams.get("code");
+
+  //url에 code가 있고 아직 supabase에 요청을 안 했을 때만 실행
+  if (code && !hasRequestedAccessToken.current) {
+    hasRequestedAccessToken.current = true;
+    const { data, error } = await supabase.functions.invoke("discord-oauth", {
+      body: { code },
+    });
+
+    if (error) {
+      console.error("에러 발생:", error);
+      alert("로그인에 실패했습니다");
+      // 에러가 나면 code는 사용하지 못하기 때문에 삭제
+      window.history.replaceState({}, document.title, "/");
+      return false;
+    } else {
+      console.log("로그인 성공!");
+      localStorage.setItem("discord_access_token", data.access_token);
+
+      // 페이지를 새로고침 하지 않고 url에 code만 지운 상태로 변경
+      window.history.replaceState({}, document.title, window.location.pathname);
+      return true;
+    }
+  }
+};
